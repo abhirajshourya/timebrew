3628485b6dcf610702621c8a44b97f86d52b3d65
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2024-05-31 11:27:09 -0400

    #14 feat: Add CRUD operations for tasks in useDatabase hook

--- hooks/useDatabase.ts
+++ hooks/useDatabase.ts
@@ -56,7 +56,7 @@ export default function useDatabase() {
     await db.execAsync('DROP TABLE IF EXISTS tasks');
 
     console.log('Database dropped');
-  }
+  };
 
   const getData = async () => {
     const [timelogs, tasks] = await Promise.all([
@@ -70,10 +70,74 @@ export default function useDatabase() {
     };
   };
 
+  /**
+   * ****************************************************
+   * **************** CRUD Operations *******************
+   * 1. Task
+   * 2. Timelog
+   * 3. Tag
+   * 4. TimelogTag
+   * ****************************************************
+   */
+
+  /**
+   * ****************************************************
+   * 1. Task
+   * ****************************************************
+   */
+  /**
+   * Create a new task and return the id
+   * @param description - Task description
+   */
+  const createTask = async (description: string) => {
+    const task = await db.runAsync('INSERT INTO tasks (description) VALUES ($description)', {
+      $description: description,
+    });
+
+    return task.lastInsertRowId;
+  };
+
+  /**
+   * Get a task by id
+   * @param id - Task id
+   * @returns - Task
+   */
+  const getTask = async (id: number) => {
+    return await db.getFirstAsync<Task>('SELECT * FROM tasks WHERE id = $id', {
+      $id: id,
+    });
+  };
+
+  /**
+   * Update a task description
+   * @param id - Task id
+   * @param description - New task description
+   */
+  const updateTask = async (task: Task) => {
+    await db.runAsync('UPDATE tasks SET description = $description WHERE id = $id', {
+      $id: task.id,
+      $description: task.description,
+    });
+  };
+
+  /**
+   * Delete a task by id
+   * @param id - Task id
+   */
+  const deleteTask = async (id: number) => {
+    await db.runAsync('DELETE FROM tasks WHERE id = $id', {
+      $id: id,
+    });
+  };
+
   return {
     getData,
     fillSampleData,
     clearData,
     dropDB,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
   };
 }
