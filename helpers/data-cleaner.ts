e7e00252e5fc0cce8e49ae9001925f2f9f8180d9
DannyGlade <darshannariya12@gmail.com>
2024-06-16 15:11:31 -0400

    refactor: Clean up data-cleaner.ts file

---
+++ helpers/data-cleaner.ts
@@ -0,0 +1,16 @@
+import { Timelog } from '@/constants/types'
+import { DataSet } from '@/components/graphs/LineGraph'
+import { formatDateToDayofWeek } from './time-format'
+
+export function cleanTimelogsForChart(timelogs: Timelog[]): DataSet {
+    const data: number[] = []
+    const labels: number[] = []
+
+    timelogs.forEach((log) => {
+        data.push(log.duration)
+        labels.push(log.start_time)        
+    })
+
+    return { data, labels }
+
+}
