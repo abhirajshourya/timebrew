be117b52c940058ca69fc27a334faae54e1b7381
DannyGlade <darshannariya12@gmail.com>
2024-06-16 15:11:31 -0400

    refactor: Add formatDateToMonth helper function for formatting date to month

--- helpers/time-format.ts
+++ helpers/time-format.ts
@@ -57,6 +57,17 @@ export const formatDateToDayofWeek = (date: number | string) => {
 }
 
 /**
+ * Format date to month
+ * @param date - Date in number or string format
+ * @returns Formatted date string
+ */
+export const formatDateToMonth = (date: number | string) => {
+    const dateObj = new Date(date)
+
+    return dateObj.toLocaleDateString('en-US', { month: 'short' })
+}
+
+/**
  * Check if the date is in the current week
  * @param date
  * @returns
@@ -65,12 +76,33 @@ export const isInThisWeek = (date: number | string) => {
     const today = new Date()
     const dateObj = new Date(date)
 
-    const todayWeek = today.getDay()
-    const dateWeek = dateObj.getDay()
+    const todayWeek = getWeekNumber(today)
+    const dateWeek = getWeekNumber(dateObj)
+    console.log(todayWeek, dateWeek)
 
     return todayWeek === dateWeek
 }
 
+/**
+ * Get the week number of the year
+ * @param date
+ * @returns
+ */
+const getWeekNumber = (date: Date) => {
+    const d = new Date(
+        Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    )
+    const dayNum = d.getUTCDay() || 7
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum)
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1))
+    return Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7)
+}
+
+/**
+ * Capitalize the first letter of a string
+ * @param str - String to capitalize
+ * @returns Capitalized string
+ */
 export const isToday = (date: number | string) => {
     const today = new Date()
     const dateObj = new Date(date)
