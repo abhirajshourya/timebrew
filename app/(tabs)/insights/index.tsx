3400e17b691a2e7e408fa232b94e2eef21ef439e
DannyGlade <darshannariya12@gmail.com>
2024-06-16 15:11:31 -0400

    refactor: Update BarGraph to include day of week label

--- app/(tabs)/insights/index.tsx
+++ app/(tabs)/insights/index.tsx
@@ -1,7 +1,11 @@
 import DropDownPicker from '@/components/form/DropDownPicker'
-
 import BarGraph, { DataSet } from '@/components/graphs/BarGraph'
-import { cleanTimelogsForChart } from '@/helpers/data-cleaner'
+import TagStat from '@/components/graphs/TagStat'
+import { TagDataset } from '@/constants/types'
+import {
+    cleanTagsTimelogs,
+    cleanTimelogsForChart,
+} from '@/helpers/data-cleaner'
 import { capitalizeFirstLetter } from '@/helpers/text-helpers'
 import useDatabase from '@/hooks/useDatabase'
 import React, { useEffect, useState } from 'react'
@@ -10,13 +14,28 @@ import { SafeAreaView, StyleSheet, View, Text, ScrollView } from 'react-native'
 type Duration = 'today' | 'week' | 'month' | 'year' | 'all'
 
 const Index = () => {
-    const { getTimeLogs } = useDatabase()
+    const { getTimeLogs, getTags, getTimelogByTag } = useDatabase()
     const [timelogs, setTimelogs] = useState<DataSet>({ data: [], labels: [] })
     const [selectedDuration, setSelectedDuration] = useState('week' as Duration)
+    const [tagsStats, setTagsStats] = useState([] as TagDataset[])
 
     useEffect(() => {
         getTimeLogs({ forThis: selectedDuration }).then((logs) => {
             setTimelogs(cleanTimelogsForChart(logs))
+        })
+
+        getTags().then((tags) => {
+            setTagsStats([])
+            tags.forEach((tag) => {
+                let tagStat = {} as TagDataset
+
+                getTimelogByTag(tag.id, { forThis: selectedDuration }).then(
+                    (timelogs) => {
+                        tagStat = cleanTagsTimelogs(tag, timelogs)
+                        setTagsStats((prev) => [...prev, tagStat])
+                    }
+                )
+            })
         })
     }, [selectedDuration])
 
@@ -24,61 +43,97 @@ const Index = () => {
         <SafeAreaView>
             <View style={styles.container}>
                 <Text style={styles.heading}>Insights</Text>
-                <ScrollView
-                    style={styles.scrollView}
-                    contentContainerStyle={styles.scrollView}
-                >
-                    <View style={{ marginBottom: 20 }}>
-                        {timelogs.data.length === 0 && (
-                            <Text>No data to show</Text>
-                        )}
-                        <BarGraph
-                            style={{ zIndex: 1 }}
-                            color="#005c99"
-                            dataSet={timelogs}
-                            // dataSet={{
-                            //     data: [
-                            //         12, 5, 9, 30, 20, 51, 20, 10, 10, 20, 15,
-                            //         10,
-                            //     ],
-                            //     labels: [
-                            //         'Jan',
-                            //         'Feb',
-                            //         'Mar',
-                            //         'Apr',
-                            //         'May',
-                            //         'Jun',
-                            //         'Jul',
-                            //         'Aug',
-                            //         'Sep',
-                            //         'Oct',
-                            //         'Nov',
-                            //         'Dec',
-                            //     ],
-                            // }}
-                            stat="120k"
-                        />
-                    </View>
-                    <View>
-                        <View>
-                            <Text
-                                style={[
-                                    { marginBottom: 10 },
-                                    styles.selectDurationTag,
-                                ]}
-                            >
-                                Duration
-                            </Text>
-                        </View>
-                        <DropDownPicker
-                            style={{ minWidth: 100 }}
-                            items={['Today', 'Week', 'Month', 'Year', 'All']}
-                            selectedValue={capitalizeFirstLetter(
-                                selectedDuration
+                <ScrollView contentContainerStyle={styles.scrollView}>
+                    <View style={{ marginBottom: 200 }}>
+                        <View style={{ marginBottom: 20 }}>
+                            {timelogs.data.length === 0 && (
+                                <Text>No data to show</Text>
                             )}
-                            setValue={setSelectedDuration as any}
-                            placeholder="Select a duration"
-                        />
+                            <BarGraph
+                                style={{ zIndex: 1 }}
+                                color="#005c99"
+                                dataSet={timelogs}
+                                // dataSet={{
+                                //     data: [
+                                //         12, 5, 9, 30, 20, 51, 20, 10, 10, 20, 15,
+                                //         10,
+                                //     ],
+                                //     labels: [
+                                //         'Jan',
+                                //         'Feb',
+                                //         'Mar',
+                                //         'Apr',
+                                //         'May',
+                                //         'Jun',
+                                //         'Jul',
+                                //         'Aug',
+                                //         'Sep',
+                                //         'Oct',
+                                //         'Nov',
+                                //         'Dec',
+                                //     ],
+                                // }}
+                                stat="120k"
+                            />
+                        </View>
+                        <View style={{ marginBottom: 20 }}>
+                            <View>
+                                <Text
+                                    style={[
+                                        { marginBottom: 10 },
+                                        styles.selectDurationLabel,
+                                    ]}
+                                >
+                                    Duration
+                                </Text>
+                            </View>
+                            <DropDownPicker
+                                style={{ minWidth: 100 }}
+                                items={[
+                                    'Today',
+                                    'Week',
+                                    'Month',
+                                    'Year',
+                                    'All',
+                                ]}
+                                selectedValue={capitalizeFirstLetter(
+                                    selectedDuration
+                                )}
+                                setValue={setSelectedDuration as any}
+                                placeholder="Select a duration"
+                            />
+                        </View>
+                        <View>
+                            <Text style={styles.insightLabel}>
+                                {selectedDuration.toLowerCase() === 'today'
+                                    ? 'Today'
+                                    : selectedDuration.toLowerCase() ===
+                                          'week' ||
+                                      selectedDuration.toLowerCase() ===
+                                          'month' ||
+                                      selectedDuration.toLowerCase() === 'year'
+                                    ? 'This ' + selectedDuration
+                                    : selectedDuration.toLowerCase() === 'all'
+                                    ? 'All time'
+                                    : ''}
+                            </Text>
+
+                            <View>
+                                {timelogs.data.length === 0 && (
+                                    <Text>No data to show</Text>
+                                )}
+                                {timelogs.data.length > 0 &&
+                                    tagsStats.map(
+                                        (tagStat, i) =>
+                                            tagStat.timeLogs.length > 0 && (
+                                                <TagStat
+                                                    key={i}
+                                                    tagStat={tagStat}
+                                                />
+                                            )
+                                    )}
+                            </View>
+                        </View>
                     </View>
                 </ScrollView>
             </View>
@@ -102,7 +157,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         minHeight: '100%',
     },
-    selectDurationTag: {
+    selectDurationLabel: {
         fontSize: 20,
         // marginBottom: 10
     },
@@ -112,6 +167,11 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
         // marginBottom: 20,
+    },
+    insightLabel: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
 })
 
