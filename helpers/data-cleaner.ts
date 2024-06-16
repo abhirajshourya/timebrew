9a413b15648f37075cbb98e06f8a86ca596d783a
DannyGlade <darshannariya12@gmail.com>
2024-06-16 15:11:31 -0400

    refactor: Clean up data-cleaner.ts and add support for cleaning tags timelogs

--- helpers/data-cleaner.ts
+++ helpers/data-cleaner.ts
@@ -1,16 +1,30 @@
-import { Timelog } from '@/constants/types'
+import { Tag, TagDataset, Timelog } from '@/constants/types'
 import { DataSet } from '@/components/graphs/BarGraph'
 import { formatDateToDayofWeek } from './time-format'
 
+/**
+ * Clean timelogs data for chart
+ * @param timelogs - Array of timelogs
+ * @returns DataSet
+ */
 export function cleanTimelogsForChart(timelogs: Timelog[]): DataSet {
     const data: number[] = []
     const labels: number[] = []
 
     timelogs.forEach((log) => {
         data.push(log.duration)
-        labels.push(log.start_time)        
+        labels.push(log.start_time)
     })
 
     return { data, labels }
+}
 
+export function cleanTagsTimelogs(tag: Tag, timelogs: Timelog[]): TagDataset {
+    const totalDuration = timelogs.reduce((acc, log) => acc + log.duration, 0)
+
+    return {
+        tag,
+        totalDuration,
+        timeLogs: timelogs,
+    }
 }
