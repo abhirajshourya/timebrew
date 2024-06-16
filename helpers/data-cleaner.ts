6f8d55c1d9bc77714ac0de60ddc99497b0e7ec90
DannyGlade <darshannariya12@gmail.com>
2024-06-16 15:11:31 -0400

    refactor: Update data-cleaner.ts to include taskIds in cleanTimelogsForChart function and groups same dates

--- helpers/data-cleaner.ts
+++ helpers/data-cleaner.ts
@@ -10,13 +10,15 @@ import { formatDateToDayofWeek } from './time-format'
 export function cleanTimelogsForChart(timelogs: Timelog[]): DataSet {
     const data: number[] = []
     const labels: number[] = []
+    const taskIds: number[] = []
 
     timelogs.forEach((log) => {
         data.push(log.duration)
-        labels.push(log.start_time)
+        labels.push(new Date(log.start_time).setHours(0, 0, 0, 0))
+        taskIds.push(log.task_id)
     })
 
-    return { data, labels }
+    return { data, labels, taskIds }
 }
 
 export function cleanTagsTimelogs(tag: Tag, timelogs: Timelog[]): TagDataset {
