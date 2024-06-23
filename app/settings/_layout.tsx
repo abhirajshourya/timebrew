917dc7660895de5bb66a171bd3c63eda09f7a986
DannyGlade <darshannariya12@gmail.com>
2024-06-22 22:19:21 -0600

    created settings files

---
+++ app/settings/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router/stack'
+
+export default function StackLayout() {
+    Stack.defaultProps = {
+        initialRouteName: 'index',
+        screenOptions: {
+            headerShown: false,
+        },
+    }
+
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{ headerShown: false, title: 'Settings' }}
+            />
+        </Stack>
+    )
+}
---
+++ app/settings/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Settings = () => {
+    return (
+        <View>
+            <Text> Settings</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Settings;
