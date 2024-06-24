2be78ac259f79319157e6a18c0c566d2b3662374
DannyGlade <darshannariya12@gmail.com>
2024-06-24 12:31:24 -0600

    refactor: Update TimeLogModal component to use Tamagui Sheet and Handle components

--- components/Modals/TimeLogModal.tsx
+++ components/Modals/TimeLogModal.tsx
@@ -2,12 +2,14 @@ import { Ionicons } from '@expo/vector-icons'
 import React, { PropsWithChildren } from 'react'
 import {
     StyleSheet,
-    View,
-    Text,
+    // View,
+    // Text,
     Modal,
     ModalProps,
     Pressable,
 } from 'react-native'
+import { Text, View } from 'tamagui'
+import { Sheet, useSheet } from '@tamagui/sheet'
 
 type TimeLogModalProps = PropsWithChildren<ModalProps> & {
     isVisible: boolean
@@ -21,41 +23,74 @@ const TimeLogModal = ({
     children,
     title,
 }: TimeLogModalProps) => {
+    const [position, setPosition] = React.useState(0)
     return (
-        <Modal
-            visible={isVisible}
-            animationType="slide"
-            transparent={true}
-            style={styles.modal}
-            onRequestClose={onClose}
-        >
-            <View style={styles.modalContent}>
-                <View style={styles.modalHeader}>
-                    <Text style={styles.modalHeaderText}>
-                        {title || 'Time Log'}
-                    </Text>
+        // <Modal
+        //     visible={isVisible}
+        //     animationType="slide"
+        //     transparent={true}
+        //     style={styles.modal}
+        //     onRequestClose={onClose}
+        // >
+        //     <View style={styles.modalContent}>
+        //         <View style={styles.modalHeader}>
+        //             <Text style={styles.modalHeaderText}>
+        //                 {title || 'Time Log'}
+        //             </Text>
 
-                    <Pressable onPress={onClose}>
-                        <Ionicons name="close" size={24} />
-                    </Pressable>
+        //             <Pressable onPress={onClose}>
+        //                 <Ionicons name="close" size={24} />
+        //             </Pressable>
+        //         </View>
+        //         <View style={styles.modalBody}>{children}</View>
+        //     </View>
+        // </Modal>
+        <Sheet
+            forceRemoveScrollEnabled={true}
+            modal={true}
+            open={isVisible}
+            onOpenChange={onClose}
+            dismissOnSnapToBottom={true}
+            position={position}
+            onPositionChange={setPosition}
+            zIndex={100_000}
+            animation="medium"
+        >
+            <Sheet.Overlay
+                animation={'lazy'}
+                enterStyle={{ opacity: 0 }}
+                exitStyle={{ opacity: 0 }}
+            />
+            <Sheet.Handle />
+            <Sheet.Frame style={styles.modal}>
+                <View style={styles.modalContent}>
+                    <View style={styles.modalHeader}>
+                        <Text style={styles.modalHeaderText}>
+                            {title || 'Time Log'}
+                        </Text>
+
+                        <Pressable onPress={onClose}>
+                            <Ionicons name="close" size={24} />
+                        </Pressable>
+                    </View>
+                    <View style={styles.modalBody}>{children}</View>
                 </View>
-                <View style={styles.modalBody}>{children}</View>
-            </View>
-        </Modal>
+            </Sheet.Frame>
+        </Sheet>
     )
 }
 
 const styles = StyleSheet.create({
     modal: {
-        borderTopWidth: 1,
-        borderTopColor: 'grey',
-        height: '100%',
+        // borderTopWidth: 1,
+        // borderTopColor: 'grey',
+        // height: '100%',
     },
     modalContent: {
-        flex: 1,
+        // flex: 1,
         justifyContent: 'flex-end',
         // alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        // backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     modalHeader: {
         flexDirection: 'row',
@@ -63,7 +98,7 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: 'white',
         width: '100%',
-        borderTopWidth: 1,
+        // borderTopWidth: 1,
         borderTopColor: 'grey',
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
