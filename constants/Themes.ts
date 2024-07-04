a19836bed5f3612f67141b5bba2125f987d7a6a5
DannyGlade <darshannariya12@gmail.com>
2024-07-04 14:46:18 -0600

    refactor: Update Themes.ts to import necessary dependencies and useColorScheme hook

--- constants/Themes.ts
+++ constants/Themes.ts
@@ -1,4 +1,9 @@
-import { createSoftenMask, createThemeBuilder } from '@tamagui/theme-builder'
+import { Theme as NavigationThemeType } from '@react-navigation/native/src/types'
+import { useState } from 'react'
+import { useMMKVString } from 'react-native-mmkv'
+import { useTheme } from 'tamagui'
+import { Theme } from './types'
+import { useColorScheme } from 'react-native'
 
 export const ThemesColors = [
     'orange',
