5f4af0cf57062434a68dc1fa949dd3adafced2f5
DannyGlade <darshannariya12@gmail.com>
2024-07-04 18:10:57 -0600

    refactor: added segment update in useEffect

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -4,6 +4,7 @@ import useDatabase from '@/hooks/useDatabase'
 import { Feather } from '@expo/vector-icons'
 import { NativeStackNavigationHelpers } from '@react-navigation/native-stack/lib/typescript/src/types'
 import { Edit3 } from '@tamagui/lucide-icons'
+import { useSegments } from 'expo-router'
 import React, { useEffect } from 'react'
 import { TouchableHighlight } from 'react-native'
 import { Button, Card, H4, H5, H6, Text, View, XStack, YStack } from 'tamagui'
@@ -16,10 +17,11 @@ interface TaskProps {
 const TaskCard = ({ task, navigation }: TaskProps) => {
     const { getTotalTimelogForTask } = useDatabase()
     const [totalTime, setTotalTime] = React.useState(0)
+    const segment = useSegments()
 
     useEffect(() => {
         getTotalTimelogForTask(task.id).then(setTotalTime)
-    })
+    }, [segment])
 
     const handleEdit = () => {
         navigation.navigate('EditTask', { task })
